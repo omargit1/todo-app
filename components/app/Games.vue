1403c33fa5d42365bf3175952cdc02fadd9415dc
oelassri <elassri.omar@gmail.com>
2022-06-10 17:09:31 +0200

    disable when clicked

--- components/app/Games.vue
+++ components/app/Games.vue
@@ -1,32 +1,29 @@
 <template>
   <v-container class="grey lighten-5">
-      <v-row no-gutters align="center">
-        <template v-for="n in cases">
-          <v-col :key="n.num" class="col-3" align="center" >
-            <v-card @click.stop="isClicked(n)">
-                <v-card-title class="case" :style="{'backgroundColor' : n.num == numClickedCurrent || n.isActive ? n.color : 'blue' }"> 
-                 {{ n.isActive ? (  n.color == 'green' ? '+'+ n.num  : '-' + n.num) : '' }}
-                </v-card-title>
-            </v-card>
-          </v-col>
-        </template>
-      </v-row>
-      <v-row align="center">
-          <v-col align="center" class="col-4">
-              <v-card style="height:60px">
-                  <v-card-title style="height:50px" class="justify-center">
-                    Score 
-                  </v-card-title>
-              </v-card>
-          </v-col>
-          <v-col align="center">
-            <v-card style="height:60px"> 
-                <v-card-title style="height:50px;font-size:3rem" class="justify-center">
-                 {{ score }}
-                </v-card-title>
-            </v-card>
-          </v-col>
-      </v-row>
+    <v-row no-gutters align="center">
+      <template v-for="n in cases">
+        <v-col :key="n.num" class="col-3" align="center">
+          <v-card @click.stop="isClicked(n)" :disabled="caseIsClicked(n)">
+            <v-card-title
+              class="case"
+              :style="{'backgroundColor' : n.num == numClickedCurrent || n.isActive ? n.color : 'blue' }"
+            >{{ n.isActive ? ( n.color == 'green' ? '+'+ n.num : '-' + n.num) : '' }}</v-card-title>
+          </v-card>
+        </v-col>
+      </template>
+    </v-row>
+    <v-row align="center">
+      <v-col align="center" class="col-4">
+        <v-card style="height:60px">
+          <v-card-title style="height:50px" class="justify-center">Score</v-card-title>
+        </v-card>
+      </v-col>
+      <v-col align="center">
+        <v-card style="height:60px">
+          <v-card-title style="height:50px;font-size:3rem" class="justify-center">{{ score }}</v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -39,10 +36,12 @@ export default {
         numClicked : null,
         colorClicked: null,
         points : 0,
+        arrayClicked :  []
     }),
     methods : {
         isClicked(c) {
             console.log('case is clicked', JSON.stringify(c))
+            this.arrayClicked.push(c.num)
             this.numClicked = c.num
             this.colorClicked = c.color
             if(c.color === 'green') {
@@ -53,6 +52,12 @@ export default {
             let ind = this.cases.findIndex(t => t.num === this.numClicked)
             this.cases[ind].isActive = true 
         },
+        caseIsClicked(n) {
+            console.log(n.num)
+           return this.arrayClicked.includes(n.num)
+             
+         
+        }
 
     },
     computed : {
@@ -98,11 +103,11 @@ export default {
 
 <style>
 .case {
-    height: 80px;
-    width: 80px;
-    background-color: grey(144, 174, 201);
-    justify-content: center;
-    margin-bottom: 5px;
-    margin-top: 5px;
+  height: 80px;
+  width: 80px;
+  background-color: grey(144, 174, 201);
+  justify-content: center;
+  margin-bottom: 5px;
+  margin-top: 5px;
 }
 </style>
